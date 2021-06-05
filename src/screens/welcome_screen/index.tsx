e8a044a8d78e2323a911e9bf80192b54aab00c5d
David Vorona <davidavorona@gmail.com>
2021-06-04 23:00:07 -0600

    Implement form event handlers

--- src/screens/welcome_screen/index.tsx
+++ src/screens/welcome_screen/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import process from "process";
 import Screen from "../../components/screen";
 import background1 from "../../../assets/images/FantasyBackground1.jpg";
@@ -27,21 +27,57 @@ for (const dependency of ["chrome", "node", "electron"]) {
 const versionText = `Powered by Node.js ${versions.node},
 Chromium ${versions.chrome}, and Electron ${versions.electron}.`;
 
-const WelcomeScreen = (): JSX.Element => (
-    <Screen style={bgStyles}>
-        <div id="title-container">
-            <h1 id="welcome-title">DnD Campaign Booklet</h1>
-        </div>
-        <div id="new-setting-wrapper" className="flex-column">
-            <input
-                id="new-setting-input"
-                placeholder="Create a setting..."
-            />
-        </div>
-        <div id="bottom-right-text" className="absolute flex-row">
-            <span>{versionText}</span>
-        </div>
-    </Screen>
-);
+type WelcomeScreenProps = Record<string,unknown>;
+interface WelcomeScreenState {
+    value: string
+}
+
+class WelcomeScreen extends Component<WelcomeScreenProps, WelcomeScreenState> {
+    constructor(props:  WelcomeScreenProps) {
+        super(props);
+        this.state = {
+            value: ""
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+        this.setState({ value: e.target.value });
+    };
+
+    handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
+        alert("Your setting has been created!");
+        e.preventDefault();
+    };
+
+    render(): React.ReactNode {
+        const { value } = this.state;
+        return (
+            <Screen style={bgStyles}>
+                <div id="title-container">
+                    <h1 id="welcome-title">DnD Campaign Booklet</h1>
+                </div>
+                <div id="new-setting-wrapper" className="flex-column">
+                    <form onSubmit={this.handleSubmit}>
+                        <input
+                            id="new-setting-input"
+                            name="new-setting"
+                            type="text"
+                            placeholder="Name your setting..."
+                            value={value}
+                            onChange={this.handleChange}
+                            autoFocus
+                        />
+                    </form>
+                </div>
+                <div id="bottom-right-text" className="absolute flex-row">
+                    <span>{versionText}</span>
+                </div>
+            </Screen>
+        );
+    }
+}
 
 export default WelcomeScreen;
